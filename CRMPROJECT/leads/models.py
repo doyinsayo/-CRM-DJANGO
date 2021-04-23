454c8ddefd4228f3cab92b3e07f023b7d783adf6
Doyinsayo Otufadebo <64381567+doyinsayo@users.noreply.github.com>
2021-04-23 15:55:06 +0100

    models

--- CRMPROJECT/leads/models.py
+++ CRMPROJECT/leads/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Lead(models.Model):
+
+    SOURCE_CHOICES = (
+        ('Youtube','Youtube'),
+        ('Google','Google'),
+        ('Twitter','Twitter'),
+        ('Others','Others'),
+    )
+
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    age = models.IntegerField(default=0)
+
+    phoned = models.BooleanField(default=False)
+    source = models.CharField(choices=SOURCE_CHOICES,max_length=100)
+    
+    profile_picture = models.ImageField(blank=True, null=True)
+    special_files = models.FileField(blank=True, null=True)
