c7bd6fbcbae02f2a9275ed3ae5f060496482cf06
Doyinsayo Otufadebo <64381567+doyinsayo@users.noreply.github.com>
2021-04-30 00:04:32 +0100

    CRUD views , static , urls

--- CRMPROJECT/leads/urls.py
+++ CRMPROJECT/leads/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
 import leads
 from leads import views
-from .views import lead_list
+from .views import lead_list,lead_detail,lead_create,lead_update, lead_delete,
+LeadListView,LeadDetailview,LeadCreateView,LeadUpdateView,LeadDeleteView
 
 
 app_name = 'leads'
 
 urlpatterns = [
-    path('',views.lead_list,name='lead_list'),
+    path('',LeadListView.as_view(),name='lead_list'),
+    path('create/',LeadCreateView.as_view(),name='lead_create'),
+    path('<int:pk>/',LeadDetailview.as_view(),name='lead_detail'),
+    path('<int:pk>/update/',LeadUpdateView.as_view(),name='lead_update'),
+    path('<int:pk>/delete/',LeadDeleteView.as_view(),name='lead_delete'),
 ]
